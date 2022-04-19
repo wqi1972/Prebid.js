07d1d48bb6de916adcc77b85a6778e1f6fccfa31
Wu, Qi <Wu.Qi@move.com>
2022-04-19 14:40:24 -0700

    move AppNexusPrebidAnalytics into Prebid.js update lock file with protobufjs v5.0.3

--- modules/appnexusAnalyticsAdapter.js
+++ modules/appnexusAnalyticsAdapter.js
@@ -4,6 +4,7 @@
 
 import adapter from '../src/AnalyticsAdapter.js';
 import adapterManager from '../src/adapterManager.js';
+import {logEvent, sendBatch} from './AppNexusPrebidAnalytics/appnexus.js';
 
 var appnexusAdapter = adapter({
   global: 'AppNexusPrebidAnalytics',
@@ -16,5 +17,13 @@ adapterManager.registerAnalyticsAdapter({
   code: 'appnexus',
   gvlid: 32
 });
+
+/**
+ * Global function to handle events emitted from Prebid.js AnalyticsAdapter
+ */
+window.AppNexusPrebidAnalytics = function(handler, eventType, data) {
+  logEvent({ eventType, data });
+  sendBatch();
+};
 
 export default appnexusAdapter;
