9811a54c8254cc2daa1bff1a187f7f037ff1dd05
Luke DeWitt <luke.dewitt!@redspace.com>
2023-03-27 14:15:53 -0300

    add logs for debug

--- newscorp.build.js
+++ newscorp.build.js
@@ -1,19 +1,26 @@
 const { config } = require('./newscorp.conf.js');
 const { execSync } = require('child_process');
 const fs = require('fs');
+const path = require('path');
 
 const buildPath = 'prebid/dist';
 const sites = Object.keys(config);
 
 function buildBuildString(site) {
   const modules = config[site].modules.join(',');
-  return `./node_modules/.bin/gulp build --website ${site} --modules=${modules}`;
+  return `${path.resolve(
+    __dirname
+  )}/node_modules/.bin/gulp build --website ${site} --modules=${modules}`;
 }
 
 function buildPrebid() {
+  console.log('dirname', __dirname);
+  console.log('filename', __filename);
+  console.log('context', process.cwd());
   while (sites.length) {
     const site = sites.shift();
     console.log(`building prebid for: ${site}`);
+    console.log(buildPath);
     execSync(buildBuildString(site));
     fs.mkdirSync(`${buildPath}/${site}/`, { recursive: true });
     fs.renameSync(
