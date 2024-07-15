a2fa4a9f172bf4aeb24f1951cafa1654fae62fd6
Luke DeWitt <luke.dewitt!@redspace.com>
2024-07-15 09:12:50 -0300

    news corp

--- modules/appnexusBidAdapter.js
+++ modules/appnexusBidAdapter.js
@@ -1,4 +1,5 @@
 import {
+  logDetails,
   createTrackPixelHtml,
   deepAccess,
   deepClone,
@@ -411,6 +412,9 @@ export const spec = {
    */
   interpretResponse: function (serverResponse, { bidderRequest }) {
     serverResponse = serverResponse.body;
+    logDetails("interpretResponse for: " + bidderRequest.bidderCode);
+    logDetails(bidderRequest);
+    logDetails(serverResponse);
     const bids = [];
     if (!serverResponse || serverResponse.error) {
       let errorMessage = `in response for ${bidderRequest.bidderCode} adapter`;
@@ -432,6 +436,8 @@ export const spec = {
         }
       });
     }
+
+     logDetails(bids);
 
     if (serverResponse.debug && serverResponse.debug.debug_info) {
       let debugHeader = 'AppNexus Debug Auction for Prebid\n\n'
@@ -567,6 +573,12 @@ function newBid(serverBid, rtbBid, bidderRequest) {
     creativeId: rtbBid.creative_id,
     dealId: rtbBid.deal_id,
     currency: 'USD',
+    dealPriority: rtbBid.deal_priority,
+    advertiserId: rtbBid.advertiser_id,
+    brandCategoryId: rtbBid.brand_category_id,
+    buyerMemberId: rtbBid.buyer_member_id,
+    mediaTypeId: rtbBid.media_type_id,
+    mediaSubtypeId: rtbBid.media_subtype_id,
     netRevenue: true,
     ttl: 300,
     adUnitCode: bidRequest.adUnitCode,
